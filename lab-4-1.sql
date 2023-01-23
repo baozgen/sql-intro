9a64adcec4e4c60b234a7ac0ce3446353d1a4ccc
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-01-23 03:31:41 +0000

    finished lab 4

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -9,7 +9,6 @@ LIMIT 1;
 
 
 SELECT players.last_name, SUM(stats.hits)
-FROM players
-INNER JOIN stats ON stats.player_id = players.id
+FROM players INNER JOIN stats ON stats.player_id = players.id
 WHERE players.first_name = "Barry"
 AND players.last_name = "Bonds";
--- lab-4-2.sql
+++ lab-4-2.sql
@@ -24,4 +24,10 @@
 -- | Billy      | Hamilton  |
 -- | Ian        | Happ      |
 
+SELECT players.first_name, players.last_name
+FROM stats 
+    INNER JOIN players ON stats.player_id = players.id
+    INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.year = 2020
+AND teams.name = "Chicago Cubs";
 
